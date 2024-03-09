9157baacbad65045472864e9176ab307e0ec2932
KummariSaiteja <saiteja@lyrostech.com>
2024-03-09 14:16:23 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+export default function App(){
+  let menu=[
+    {id:1, name:"Americana", price:100, image:'/ecomm-react/images/Americano.jpg'},
+    {id:2, name:"Coffee", price:60, image:'/ecomm-react/images/coffe.jpg'},
+    {id:3, name:"Expresso", price:120, image:'/ecomm-react/images/expresso.jpg'},
+    {id:4, name:"Latte", price:150, image:'/ecomm-react/images/latte.jpg'},
+    {id:5, name:"Macchiato", price:100, image:'/ecomm-react/images/macchiato.jpg'},
+    {id:6, name:"Combo Snacks", price:120, image:'/ecomm-react/images/snacks.jpg'},
+];
+  return(
+    <div className="menu-container">
+   {menu.map((ele)=>(
+    <div key={ele.id} >
+    <div>
+      <img src={ele.image} alt={ele.name} />
     </div>
-  );
-}
+    <div>{ele.name}</div>
+    <div>{ele.price}</div>
+  </div>
 
-export default App;
+  ))}
+</div>
+
+  )
+
+}
